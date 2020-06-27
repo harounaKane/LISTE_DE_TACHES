09a288a5e861a2cdb28fec8b6b317eb23c53b61c
harounaKane <harouna.Kane@hotmail.fr>
2020-06-27 12:46:24 +0200

    La toute premiere version de l'application LIST_DE_TACHES

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text>La toute première version de mon APP Liste de taches !</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
     backgroundColor: '#fff',
